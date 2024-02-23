da6359213aea2732b79b1c92331994da9ae216c2
xDerrick23 <dinodidiodoro23@gmail.com>
2024-02-23 13:56:15 +0100

    Initial commit

---
+++ src/test/java/dinodidiodoro/U5W7L5/U5W7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dinodidiodoro.U5W7L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
